50a9a54927706e76ca59d5f32357e816e846218e
Paul-Lac <plnlacroix@gmail.com>
2023-12-11 17:14:38 +0100

    Exercise Quete 03

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,10 +1,22 @@
+const pokemonList = [
+    { 
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
 function PokemonCard() {
+    const pokemon = pokemonList[1];
     return (
-    <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-        <figcaption>Bulbasaur</figcaption>
+    <figure> {pokemon.imgSrc !== undefined ? <img src={pokemon.imgSrc} alt="Bulbasaur" /> : <p>???</p>}
+        <figcaption>{pokemon.name}</figcaption>
     </figure>
     );
 }
 
 export default PokemonCard;
+
