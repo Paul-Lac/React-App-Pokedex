8809852f3128fa216bede89a44de977279c066f4
Paul-Lac <plnlacroix@gmail.com>
2023-12-11 18:00:59 +0100

    Exercise Quete 04

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,19 +1,10 @@
-const pokemonList = [
-    { 
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
 
-function PokemonCard() {
-    const pokemon = pokemonList[1];
+
+function PokemonCard(props) {
+    console.log(props);
     return (
-    <figure> {pokemon.imgSrc !== undefined ? <img src={pokemon.imgSrc} alt="Bulbasaur" /> : <p>???</p>}
-        <figcaption>{pokemon.name}</figcaption>
+    <figure> {props.pokemon.imgSrc !== undefined ? <img src={props.pokemon.imgSrc} alt="Bulbasaur" /> : <p>???</p>}
+        <figcaption>{props.pokemon.name}</figcaption>
     </figure>
     );
 }
