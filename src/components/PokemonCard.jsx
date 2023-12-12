97c42a5436a9e3e4599b2a92610ba5d644b926e7
Paul-Lac <plnlacroix@gmail.com>
2023-12-12 10:45:45 +0100

    Exercise Quete 05

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,13 +1,20 @@
-
+import PropTypes from "prop-types";
 
 function PokemonCard(props) {
     console.log(props);
     return (
-    <figure> {props.pokemon.imgSrc !== undefined ? <img src={props.pokemon.imgSrc} alt="Bulbasaur" /> : <p>???</p>}
+    <figure> {props.pokemon.imgSrc !== undefined ? <img src={props.pokemon.imgSrc} alt={props.pokemon.name} /> : <p>???</p>}
         <figcaption>{props.pokemon.name}</figcaption>
     </figure>
     );
 }
+
+PokemonCard.propTypes = {
+    pokemon: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    imgSrc: PropTypes.string,
+    }).isRequired,
+  };
 
 export default PokemonCard;
 
